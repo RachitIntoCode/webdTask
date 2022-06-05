8470e95094e7f168a32e44478949f05a2b7d32ad
RachitIntoCode <99401616+RachitIntoCode@users.noreply.github.com>
2022-06-05 07:38:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'React';
+import './App.css';
+import card from './components/card'
+
+function App() {
+    return (
+        <div className = 'App' >
+<card title = 'Card Title' 
+imageUrl = 'https://dummyjson.com/image/i/products/1/thumbnail.jpg" alt="" class="prod-img'
+body ='hello'
+/>
+        </div>
+    );
+}
+
+export default App;
